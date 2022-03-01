d8de9cfe274d59c01d7c8cc8153ba7ef2a14aa2c
sanjaysattva <85502194+sanjaysattva@users.noreply.github.com>
2022-03-01 23:51:00 +0530

    Update main.ino

--- Encoder-Decoder/main.ino
+++ Encoder-Decoder/main.ino
@@ -1,163 +1,57 @@
 #include <Wire.h>
-int x;
 
-int red = 11;
-int blue = 10;
-int green = 9;
-int red2 = 6;
-int blue2 = 5;
-int green2 = 3;
-
+//String x;
+//char y;
+int code=0;
+int input =0;
 void setup()
 {
-  pinMode(red , OUTPUT);
-  pinMode(green , OUTPUT);
-  pinMode(blue , OUTPUT);
-  pinMode(red2 , OUTPUT);
-  pinMode(green2 , OUTPUT);
-  pinMode(blue2 , OUTPUT);
-  Wire.begin(2);
+  Wire.begin();
+  Serial.begin(9600);
+  Serial.println("ENTER 2 DIGIT  CODE");
   
-  Wire.onReceive(receiveEvent);
-}
-void receiveEvent(int bytes)
-{
-  x = Wire.read();
-  Serial.println("The encrypted code is :");
-  Serial.print(x);
-  
- //DECRYPT
-  int decrypt=0;
-  decrypt = x-11;
-  
-  Serial.println("The decrypted code is: ");
-  Serial.print(decrypt);
-  
-  int tens = decrypt/10;
-  int ones = decrypt - tens*10;
-  
-  //LED WORKING
-  //TENS PLACE
-  if(tens == 1)
-  {
-    analogWrite(red, 255);
-    analogWrite(green,200);
-    analogWrite(blue, 0);
-  }
-  if(tens == 2)
-  {
-    analogWrite(red, 255);
-    analogWrite(green,0);
-    analogWrite(blue, 0);
-  }
-  if(tens == 3)
-  {
-    analogWrite(red, 255);
-    analogWrite(green,50);
-    analogWrite(blue, 10);
-  }
-  if(tens == 7)
-  {
-    digitalWrite(red, HIGH);
-    digitalWrite(green, LOW);
-    digitalWrite(blue, HIGH);
-  }
-  if(tens==4)
-  {
-    digitalWrite(red, HIGH);
-    digitalWrite(green, HIGH);
-    digitalWrite(blue, LOW);
-  }
-  if(tens == 5)
-  {
-    digitalWrite(red, LOW);
-    digitalWrite(green, HIGH);
-    digitalWrite(blue, LOW);
-  }
-  if(tens == 6)
-  {
-    digitalWrite(red, LOW);
-    digitalWrite(green, LOW);
-    digitalWrite(blue, HIGH);
-  }
-   if(tens == 8)
-  {
-    analogWrite(red, 255);
-    analogWrite(green,189);
-    analogWrite(blue, 255);
-  }
-   if(tens == 9)
-  {
-    analogWrite(red, 255);
-    analogWrite(green,255);
-    analogWrite(blue, 255);
-  }
-  
-  //ONES PLACE
-    if(ones == 0)
-  {
-    analogWrite(red2, 0);
-    analogWrite(green2,0);
-    analogWrite(blue2, 0);
-  }
-  if(ones == 1)
-  {
-    analogWrite(red2, 255);
-    analogWrite(green2,122);
-    analogWrite(blue2, 0);
-  }
-  if(ones == 2)
-  {
-    analogWrite(red2, 255);
-    analogWrite(green2,0);
-    analogWrite(blue2, 0);
-  }
-  if(ones == 3)
-  {
-    analogWrite(red2, 255);
-    analogWrite(green2,50);
-    analogWrite(blue2, 10);
-  }
-  if(ones == 7)
-  {
-    digitalWrite(red2, HIGH);
-    digitalWrite(green2, LOW);
-    digitalWrite(blue2, HIGH);
-  }
-  if(ones==4)
-  {
-    digitalWrite(red2, HIGH);
-    digitalWrite(green2, HIGH);
-    digitalWrite(blue2, LOW);
-  }
-  if(ones == 5)
-  {
-    digitalWrite(red2, LOW);
-    digitalWrite(green2, HIGH);
-    digitalWrite(blue2, LOW);
-  }
-  if(ones == 6)
-  {
-    digitalWrite(red2, LOW);
-    digitalWrite(green2, LOW);
-    digitalWrite(blue2, HIGH);
-  }
-   if(ones == 8)
-  {
-    analogWrite(red2, 255);
-    analogWrite(green2,189);
-    analogWrite(blue2, 255);
-  }
-   if(ones == 9)
-  {
-    analogWrite(red2, 255);
-    analogWrite(green2,255);
-    analogWrite(blue2, 255);
-  }
-  
+ 
 }
 
 void loop()
 {
-  delay(5000);
+  
+   /*if (Serial.available() > 0) {
+    
+    input = Serial.read();
+   }
+   if(input == 1311)
+  {
+   Serial.println("CORRECT! ENTER 2 DIGIT ");
+   
+  }
+  /*else
+  {
+    Serial.println("INCORRECT PASSCODE");
+    
+  }*/
+  
+  
+    if (Serial.available() > 0) {
+    
+      int c = Serial.parseInt();
+      code = c;
+      Serial.println("the 2 digit code is ");
+    Serial.println(code);
+
+    //Encryption starts
+
+    int encrypt =0;
+    encrypt = code + 11;
+
+    Wire.beginTransmission(2);                                                
+    Wire.write(encrypt);                                                    
+    Wire.endTransmission();
+    Serial.println("the encrypt code is ");
+    Serial.println(encrypt);
+
+    delay(2000);
+   
+  }
+  
 }
